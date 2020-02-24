e2f66c4cb09e7a6dccf864f84e8f47273c184e3e
melyaua <igor.melnychenko@tmx.com.ua>
2020-02-24 17:03:17 +0200

    simple platformer

---
+++ script.js
@@ -0,0 +1,177 @@
+(function () {
+ var requestAnimationFrame = window.requestAnimationFrame;
+ window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var canvas = document.getElementById("canvas"),
+ ctx = canvas.getContext("2d"),
+ width = 1000,
+ height = 400,
+ player = {
+     x: width / 2,
+     y: 200,
+     width: 25,
+     height: 25,
+     speed:  5,
+     velX: 0,
+     velY: 0,
+     jumping: false,
+     grounded: false,
+     color:'#E6AC27'
+ },
+ keys = [],
+ friction = 0.8,
+ gravity = 0.4,
+ boxes = [];
+
+boxes.push({//box for the ground
+ x: 0,
+ y: height - 10,
+ width: width,
+ height: 50,
+ color: 'orange'
+});
+
+boxes.push({
+ x: 340,
+ y: 350,
+ width: 90,
+ height: 10,
+ color: '#655643'
+});
+boxes.push({
+ x: 740,
+ y: 300,
+ width: 160,
+ height: 10,
+ color: '#655643'
+});
+boxes.push({
+    x: 600,
+    y: 350,
+    width: 60,
+    height: 40,
+    color: '#655643'
+});
+
+
+canvas.width = width;
+canvas.height = height;
+
+function update() {
+ // check keys
+ if (keys[38] || keys[32] || keys[87]) {
+     // up arrow or space
+     if (!player.jumping && player.grounded) {
+         player.jumping = true;
+         player.grounded = false;
+         player.velY = -player.speed * 2.5;//how high to jump
+     }
+ }
+ if (keys[39] || keys[68]) {
+     // right arrow
+     if (player.velX < player.speed) {
+         player.velX++;
+     }
+ }
+ if (keys[37] || keys[65]) {
+     // left arrow
+     if (player.velX > -player.speed) {
+         player.velX--;
+     }
+ }
+
+
+
+ player.velX *= friction;
+ player.velY += gravity;
+
+
+//  console.log(player.velX);
+ console.log(player.velY);
+
+ ctx.clearRect(0, 0, width, height);
+ ctx.beginPath();
+ 
+ player.grounded = false;
+ for (var i = 0; i < boxes.length; i++) {//print boxes
+     ctx.fillStyle = boxes[i].color;
+     ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+     
+     var dir = colCheck(player, boxes[i]);
+
+     if (dir === "l" || dir === "r") {
+         player.velX = 0;
+         player.jumping = false;
+     } else if (dir === "b") {
+         player.grounded = true;
+         player.jumping = false;
+     } else if (dir === "t") {
+         player.velY *= -1;
+     }
+
+ }
+ 
+ if(player.grounded){
+      player.velY = 0;
+ }
+ 
+ player.x += player.velX;
+ player.y += player.velY;
+
+
+ ctx.fill();//Draw charater stuff
+ ctx.fillStyle = player.color;
+ ctx.fillRect(player.x, player.y, player.width, player.height);
+ 
+
+ requestAnimationFrame(update);
+}
+
+function colCheck(shapeA, shapeB) {
+ // get the vectors to check against
+ var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+     vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+     // add the half widths and half heights of the objects
+     hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+     hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+     colDir = null;
+
+ // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+ if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+     // figures out on which side we are colliding (top, bottom, left, or right)
+     var oX = hWidths - Math.abs(vX),
+         oY = hHeights - Math.abs(vY);
+     if (oX >= oY) {
+         if (vY > 0) {
+             colDir = "t";
+             shapeA.y += oY;
+         } else {
+             colDir = "b";
+             shapeA.y -= oY;
+         }
+     } else {
+         if (vX > 0) {
+             colDir = "l";
+             shapeA.x += oX;
+         } else {
+             colDir = "r";
+             shapeA.x -= oX;
+         }
+     }
+ }
+ return colDir;
+}
+
+document.body.addEventListener("keydown", function (e) {
+ keys[e.keyCode] = true;
+});
+
+document.body.addEventListener("keyup", function (e) {
+ keys[e.keyCode] = false;
+});
+
+
+window.addEventListener("load", function () {
+ update();
+});
